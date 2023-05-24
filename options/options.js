b114a05f00af6429c3b6379545f20a2895a4309a
Crowdbotics <team@crowdbotics.com>
2023-05-24 14:36:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-glade-40956",
+  url: "https://falling-glade-40956.botics.co",
+  api: "https://falling-glade-40956.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
